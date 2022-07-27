a6b717ae6927dbe825d75e7592f995529edbd7d9
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-27 20:02:18 +0530

    Added: Address Book For Edit Existing Contact

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -4,8 +4,10 @@ public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book Program ");
 		AddressBookOperations add = new AddressBookOperations();
-		System.out.println("Create Contact");
+
 		add.addContact();
+		System.out.println(add.display());
+		add.editContact();
 		System.out.println(add.display());
 
 	}
--- src/com/bridgelabz/AddressBookOperations.java
+++ src/com/bridgelabz/AddressBookOperations.java
@@ -26,6 +26,39 @@ public class AddressBookOperations {
 		contact.setEmailId(sc.next());
 
 	}
+	public void editContact() {
+		System.out.println("Enter the first name of person to edit:");
+		String firstName = sc.next();
+		if (firstName.equals(contact.getFirstName())) {
+			System.out.println("Edit the details of person");
+			System.out.println("Enter first name:");
+			contact.setFirstName(sc.next());
+
+			System.out.println("Enter last Name:");
+			contact.setLastName(sc.next());
+
+			System.out.println("Enter address:");
+	        contact.setAddress(sc.next());
+	        
+			System.out.println("Enter city:");
+			contact.setCity(sc.next());
+
+			System.out.println("Enter state:");
+			contact.setState(sc.next());
+
+			System.out.println("Enter zip Code:");
+			contact.setZip(sc.next());
+			
+			System.out.println("Enter phone number:");
+			contact.setPhoneNumber(sc.next());
+
+			System.out.println("Enter EmailId:");
+			contact.setEmailId(sc.next());
+
+		} else {
+			System.out.println("Person not found");
+		}
+	}
 
 	Contact display() {
 		return contact;
